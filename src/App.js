f65ba801d93ab9c9e027517ca61db1df891d2586
ricardolicea <ricardo.liceamata@gmail.com>
2021-02-10 21:01:32 -0600

    Implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Switch, Route} from 'react-router-dom';
 import Header from './components/header/header.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from'./pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
 
@@ -21,11 +21,26 @@ class App extends React.Component {
 
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
-      console.log(user)
-    })
-  }
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+          console.log(this.state)
+
+
+        });
+      }
+      this.setState({currentUser: userAuth});
+      });
+    }
+
 
   componentWillUnmount(){
     this.unsubscribeFromAuth();
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import { createContext } from 'react';
 
 const config = {
     apiKey: "AIzaSyByXs_bftwRT8miDoYHHo_oc5wgA4c2Ju0",
@@ -11,6 +12,33 @@ const config = {
     appId: "1:555343711736:web:d37c4bb4dba7cfcd1f1a0d",
     measurementId: "G-XP2ZDSSRC6"
   };
+
+  export const createUserProfileDocument = async(userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+    const snapshot = await userRef.get();
+
+    if(!snapshot.exists){
+      const {displayName, email} = userAuth;
+      const createdAt = new Date();
+
+      try{
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+      }catch(error){
+        console.log("Error creating userRef", error.message)
+      }
+    }
+    return userRef;
+
+    console.log(snapshot);
+  }
 
 firebase.initializeApp(config);
 
